6b43a23f4a5c7630e31e1c84c74f9c0fc7c75203
Juan Lizarazo <gabriellg4@gmail.com>
2014-10-20 01:34:37 -0600

    http service in NewsController

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 (function(){
   var newsFeedApp = angular.module('newsFeedApp', []);
   
-  newsFeedApp.controller('NewsController', function() {
+  newsFeedApp.controller('NewsController', ['$http', function($http) {
     this.newsFeed = [];
-  });
+  }]);
 })();
